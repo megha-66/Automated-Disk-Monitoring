4a7d08f80dde24629c0ffe97b0c54dcc6c7e3874
Megha <vaswanimegha09@gmail.com>
2025-03-23 15:30:46 +0530

    Initial commit

---
+++ diskmanagement.sh
@@ -0,0 +1,33 @@
+#!/bin/bash 
+
+# setting the variables
+THRESHOLD=90 
+ALERT_FOLDER="/home/megha_vas23"  # 
+LOGFILE="disk_usage_report.log"
+
+# function to check disk usage
+
+check_disk_usage() {
+	echo " Checking Disk usage ...." 
+	USAGE=$(df / | tail -1 | awk '{print $5}' | sed 's/%//')
+	echo " Current disk usage : $USAGE%"
+
+        if [ "$USAGE" -ge "$THRESHOLD" ]; then 
+		echo "$(date '+%Y-%m-%d %H:%M:%S')- Warning : Disk usage is at ${USAGE}%!" | tee -a $LOGFILE 
+		list_large_files
+	else
+		echo "$(date '+%Y-%m-%d %H:%M:%S')- Disk usage is under control at ${USAGE}%." | tee -a $LOGFILE 
+        fi 
+}
+
+# function to list top 10 large files/directories
+list_large_files() {
+	echo " Listing top 10 largest files/directories:" | tee -a $LOGFILE 
+	du -ah / 2>/dev/null | sort -rh | head -n 10 | tee -a $LOGFILE 
+	echo " Cleanup done." | tee -a $LOGFILE
+}
+
+check_disk_usage
+echo " Disk usage monitoring complete. Report saved to $LOGFILE."
+	 
+
